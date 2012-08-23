7ff17d56a010c2d6acb421d292db767376c37aba
dpiparo <dpiparo@4525493e-7705-40b1-a816-d608a930855b>
2012-08-23 12:54:33 +0000

    o First commit

---
+++ trunk/ReadMe.txt
@@ -0,0 +1,35 @@
+VDT is a library of mathematical functions, implemented in 
+double and single precision. The implementation is fast and 
+with the aid of modern compilers (e.g. gcc 4.7) autovectorisable.
+For more information visit:
+https://svnweb.cern.ch/trac/vdt
+ 
+The basic idea is to exploit pade polinomials. A lot of ideas were inspired by 
+the cephes math library (by Stephen L. Moshier, moshier@na-net.ornl.gov) as 
+well as portions of actual code. The Cephes library can be found here: 
+http://www.netlib.org/cephes
+
+Implemented functions
+ - log
+ - exp
+ - sin
+ - cos
+ - tan
+ - asin
+ - acos
+ - atan
+ - inverse sqrt
+ - inverse (faster than division, based on isqrt)
+
+
+To compile it:
+cmake .
+make
+
+If you would like to compile the executables necessary for the diagnostics
+(cpu and arithmetic performance measurements) type
+cmake -D DIAG=1 .
+make
+
+The executables will be put in the /bin directory.
+
---
+++ trunk/src/vector_signatures_generator.py
@@ -0,0 +1,112 @@
+#! /usr/bin/python
+
+"""
+Generate the .cc file with the signatures
+of the vector functions.
+"""
+
+RESTRICT="__restrict__"
+
+FUNCTIONS_LIST=["asin",
+                "atan",
+                "acos",
+                "cos",
+                "exp",
+                "inv",
+                "log",
+                "sin",
+                "isqrt",
+                "tan",
+                "identity",
+                "fast_asin",
+                "fast_acos",
+                "fast_atan",
+                "fast_cos",
+                "fast_exp",
+                "fast_inv",
+                "fast_approx_inv",
+                "fast_log",
+                "fast_sin",
+                "fast_isqrt",
+                "fast_approx_isqrt",
+                "fast_tan"]
+
+VDT_VECTOR_HEADER='vdtMath.h'
+VDT_VECTOR_IMPL='vdtMath_vector.cc'
+                
+#------------------------------------------------------------------
+                
+def create_vector_signature(fcn_name,is_double=False,is_impl=False):
+  # For single precision
+  suffix="fv"
+  float_suffix="f"
+  type="float"
+  if is_double:
+    suffix="v"
+    type="double"
+    float_suffix=""
+  prefix=""
+  vfcn_name="%s%s" %(fcn_name,suffix)
+  data_type="%s* %s" %(type, RESTRICT)
+  new_fcn_name="%s%s" %(prefix,fcn_name)
+  code =  "void %s%s(const uint32_t size, %s iarray, %s oarray)" %(new_fcn_name,suffix,data_type,data_type)
+  if is_impl:
+    code += "{\n"+\
+          "  for (uint32_t i=0;i<size;++i)\n"+\
+		  "    oarray[i]=%s%s(iarray[i]);\n" %(new_fcn_name,float_suffix)+\
+          "}\n\n"
+  else:
+	code += ";\n"	  
+  return code
+		 
+#------------------------------------------------------------------
+		 
+def create_vector_signatures(is_impl=False):
+  code="namespace vdt{\n"
+  for is_double in (True,False):
+    for fcn_name in sorted(FUNCTIONS_LIST):
+      code+=create_vector_signature(fcn_name,is_double,is_impl)
+  code += "} // end of vdt namespace"
+  return code
+
+
+#------------------------------------------------------------------   
+   
+def get_header_file():
+  code= "// Automatically generated\n"\
+        '#ifndef __VDT_VECTOR__\n'\
+        '#define __VDT_VECTOR__\n'\
+        '#include "vdtMath.h"\n'\
+        '#include "inttypes.h"\n'+\
+        create_vector_signatures(is_impl=False)+\
+        '\n'\
+        '#endif'
+  return code
+
+#------------------------------------------------------------------   
+		   
+def get_impl_file():
+  code= "// Automatically generated\n"+\
+        '#include "%s"\n' %VDT_VECTOR_HEADER+\
+        create_vector_signatures(is_impl=True)
+  return code
+		  
+#------------------------------------------------------------------
+
+def create_header():
+  ofile=file(VDT_VECTOR_HEADER,'w')
+  ofile.write(get_header_file())
+  ofile.close()
+  
+#------------------------------------------------------------------
+
+def create_impl():
+  ofile=file(VDT_VECTOR_IMPL,'w')
+  ofile.write(get_impl_file())
+  ofile.close()
+
+#------------------------------------------------------------------
+  
+if __name__ == "__main__":
+  #create_header()
+  create_impl()

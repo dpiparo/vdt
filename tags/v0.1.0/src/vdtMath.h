a5409032a695d2048023cdf4982f223cfb2f4151
dpiparo <dpiparo@4525493e-7705-40b1-a816-d608a930855b>
2012-08-24 07:31:41 +0000

    o correct svml settings in cmake

---
+++ tags/v0.1.0/src/vdtMath.h
@@ -0,0 +1,17 @@
+// Include all the VDT fucntions
+
+#include "sin.h"
+#include "cos.h"
+#include "tan.h"
+
+#include "asin.h"
+#include "atan.h"
+
+#include "exp.h"
+#include "log.h"
+
+#include "sqrt.h"
+
+#include "inv.h"
+
+#include "identity.h"

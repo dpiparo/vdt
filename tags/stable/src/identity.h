5a2dde026ed3cc4b468a79bd11ce940700f008bb
dpiparo <dpiparo@4525493e-7705-40b1-a816-d608a930855b>
2012-09-12 13:58:48 +0000

    o new stable version

---
+++ tags/stable/src/identity.h
@@ -0,0 +1,53 @@
+/*
+ * identity.h
+ * For testing purposes
+ * 
+ *  Created on: Jun 23, 2012
+ *      Author: Danilo Piparo, Thomas Hauth, Vincenzo Innocente
+ */
+
+/* 
+ * VDT is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ * 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser Public License for more details.
+ * 
+ * You should have received a copy of the GNU Lesser Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#ifndef IDENTITY_H_
+#define IDENTITY_H_
+
+#include "vdtcore_common.h"
+#include "sincos_common.h"
+
+#include <cmath>
+#include <limits>
+
+namespace vdt{
+
+using namespace sincos;
+
+// Identity double precision ---------------------------------------------------
+/// Double implementation
+inline double identity(double x){return x;}
+
+// Identity float precision ---------------------------------------------------
+/// Double implementation
+inline float identityf(float x){return x;}
+
+//------------------------------------------------------------------------------
+
+void identityv(const uint32_t size, double* __restrict__ iarray, double* __restrict__ oarray);
+void identityfv(const uint32_t size, float* __restrict__ iarray, float* __restrict__ oarray);
+
+
+} //vdt namespace
+
+#endif /* IDENTITY_H_ */

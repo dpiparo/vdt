d30415325dfd17497b41e6441cfc68d7a2883bac
dpiparo <dpiparo@4525493e-7705-40b1-a816-d608a930855b>
2012-11-05 23:28:57 +0000

    o Towards inclusion of atan2 in the tests

--- trunk/progs/units/atan2_test.cpp
+++ trunk/progs/units/atan2_test.cpp
@@ -7,24 +7,14 @@
 using namespace vdt;
 using namespace vdth;
 
-// Temporary to "flatten" the atan2
-inline float flat_fast_atan2f(float x){
-  return fast_atan2f(x,x/2+1);
-}
-
-inline float flat_atan2(float x){
-  return atan2(x,x/2+1);
-}
-
-
-
 int main(){
 
 	constexpr uint32_t size=10;
 	
 	//sp 
-    float spvals[size]={-1e30f,-1e19f,-300.f,-20.f,0.f,13.f,230.f,1e20f,1e30f};
-    printFuncDiff("atanf", (spspfunction)flat_atan2,(spspfunction)flat_fast_atan2f,spvals,size);
+    float spvals1[size]={-1e30f,-1e19f,-300.f,-20.f,-0.23f,0.f,13.f,230.f,1e20f,1e30f};
+    float spvals2[size]={1e2f,1e1f,400.f,20.f,0.934f,0.f,-21.2f,-532.f,-1e17f,-1e29f};
+    printFuncDiff("atan2f", (spsp2function)atan2,(spsp2function)fast_atan2f,spvals1,spvals2,size);
 }
 
 #endif /* ATAN2_TEST_CPP_ */

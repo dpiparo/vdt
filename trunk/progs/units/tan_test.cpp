2d4cbf46495076e8ea35295f74dfff9540193083
dpiparo <dpiparo@4525493e-7705-40b1-a816-d608a930855b>
2013-03-21 00:00:17 +0000

    o Fixed tan value for x==0

--- trunk/progs/units/tan_test.cpp
+++ trunk/progs/units/tan_test.cpp
@@ -20,7 +20,7 @@ int main(){
 	constexpr double PI = M_PI;
 	
 	// dp
-	double dpvals[size]={0.01, 0.1+PI/4, 0.01+PI/2, 1176.2*PI,PI/7,-PI/7.5,PI/85,-PI/19,PI/10,-0.000001};
+	double dpvals[size]={0.01, 0.1+PI/4, 0.01+PI/2, 1176.2*PI,0,-PI/7.5,PI/85,-PI/19,PI/10,-0.000001};
 	printFuncDiff("tan", (dpdpfunction)tan,(dpdpfunction)fast_tan,dpvals,size);
 	printFuncDiff ("tanv", (dpdpfunctionv) tanv, (dpdpfunctionv) fast_tanv, dpvals, size );
 	

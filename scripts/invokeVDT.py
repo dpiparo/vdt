8e18d56f4b8542beb43597a560012ca55e1a6989
Vincenzo Innocente <vincenzo.innocente@cern.ch>
2017-04-01 17:09:12 +0200

    support double and single

--- scripts/invokeVDT.py
+++ scripts/invokeVDT.py
@@ -1,24 +1,38 @@
 import numpy
 import os
+from ctypes import c_char_p
 
-def invokef(vi,f) :
+def vdt_arch() :
+   _path = os.path.dirname('__file__')
+   lib = numpy.ctypeslib.load_library('vdtdiag_numpyWrapper', _path)
+   f = lib['vdt_arch']
+   f.restype = c_char_p
+   return f()
+
+def invoke(vi,f,type) :
   requires = ['CONTIGUOUS', 'ALIGNED']
   vi = numpy.asanyarray(vi)
-  vi = numpy.require(vi, numpy.single, requires)
+  vi = numpy.require(vi, type, requires)
   vo = numpy.empty_like(vi)
   f(vi,vo,vi.size)
   return vo
 
-def loadInvokef(vi,fn) :
-   fn+='fv'
+def loadInvoke(vi,fn,is_single) :
+   if (is_single) :
+     fn+='fv'
+     type = numpy.single
+   else :
+     fn+='v'
+     type = numpy.float      
+
    _path = os.path.dirname('__file__')
    lib = numpy.ctypeslib.load_library('vdtdiag_numpyWrapper', _path)
    f = lib[fn]
    f.restype = None
-   f.argtypes = [numpy.ctypeslib.ndpointer(numpy.single,
+   f.argtypes = [numpy.ctypeslib.ndpointer(type,
                       flags='aligned, contiguous'),
-                 numpy.ctypeslib.ndpointer(numpy.single,
+                 numpy.ctypeslib.ndpointer(type,
                       flags='aligned, contiguous,writeable'),
                  numpy.ctypeslib.c_intp]
-   return invoke(vi,f)
+   return invoke(vi,f,type)
 

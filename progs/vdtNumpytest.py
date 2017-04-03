d3cd860a170af820d25cb2930a2698b88449fba5
Vincenzo Innocente <vincenzo.innocente@cern.ch>
2017-04-03 12:02:32 +0200

    longer loop

--- progs/vdtNumpytest.py
+++ progs/vdtNumpytest.py
@@ -8,7 +8,7 @@ print vdt_sinf([1,2,3,4])
 print vdt_atan2([1,2,-3,-4],[1,-2,3,-4])
 print vdt_sincosf([1,2,3,4])
 
-x = np.linspace(-np.pi, np.pi, 201)
+x = np.linspace(-np.pi, np.pi, 2001)
 
 def nsc() :
     global x
@@ -18,8 +18,14 @@ def nsc() :
 
 def vsc() :
     global x
+    return vdt_sincos(x)
+
+def vscf() :
+    global x
     return vdt_sincosf(x)
 
 
-print(timeit.timeit("nsc()", setup="from __main__ import nsc"))
-print(timeit.timeit("vsc()", setup="from __main__ import vsc"))
+print(timeit.timeit("nsc()", setup="from __main__ import nsc",number=100000))
+print(timeit.timeit("vsc()", setup="from __main__ import vsc",number=100000))
+print(timeit.timeit("vscf()", setup="from __main__ import vscf",number=100000))
+

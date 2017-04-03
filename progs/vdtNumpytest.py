fddf8aebb3269db1d8a3109eaaa3b1c88de382d7
Vincenzo Innocente <vincenzo.innocente@cern.ch>
2017-04-03 14:19:15 +0200

    add exp

--- progs/vdtNumpytest.py
+++ progs/vdtNumpytest.py
@@ -7,6 +7,8 @@ print vdt_sin([1,2,3,4])
 print vdt_sinf([1,2,3,4])
 print vdt_atan2([1,2,-3,-4],[1,-2,3,-4])
 print vdt_sincosf([1,2,3,4])
+print vdt_expf([1,2,3,4])
+
 
 xx = np.linspace(-np.pi, np.pi, 2001)
 xf = np.linspace(-np.pi, np.pi, 2001)
@@ -34,7 +36,29 @@ def vscf() :
     global x
     return vdt_sincosf(x)
 
+
+def nex() :
+    global xx
+    return np.exp(xx)
+
+def nexf() :
+    global xf
+    return np.sin(xf)
+
+
+def vex() :
+    global x
+    return vdt_exp(x)
+
+def vexf() :
+    global x
+    return vdt_expf(x)
+
+
+
 (fscf, ftype) = VDTFunMap['vdt_sincosf']
+(fexf, ftype) = VDTFunMap['vdt_expf']
+
 
 requires = ['CONTIGUOUS', 'ALIGNED']
 x = numpy.asanyarray(x)
@@ -49,11 +73,26 @@ xf = numpy.require(xf, ftype, requires)
 def vscff() :
     global x
     global fscf
-    global ftype
     global vo1
     global vo2
-    return fscf(x,vo1,vo2,x.size)
+    fscf(x,vo1,vo2,x.size)
 
+def vexff() :
+    global x
+    global fscf
+    global vo1
+    fexf(x,vo1,x.size)
+
+
+print "timing exp"
+print(timeit.timeit("nex()", setup="from __main__ import nex",number=100000))
+print(timeit.timeit("nexf()", setup="from __main__ import nexf",number=100000))
+print(timeit.timeit("vex()", setup="from __main__ import vex",number=100000))
+print(timeit.timeit("vexf()", setup="from __main__ import vexf",number=100000))
+print(timeit.timeit("vexff()", setup="from __main__ import vexff",number=100000))
+
+
+print "timing sincos"
 print(timeit.timeit("nsc()", setup="from __main__ import nsc",number=100000))
 print(timeit.timeit("nscf()", setup="from __main__ import nscf",number=100000))
 print(timeit.timeit("vsc()", setup="from __main__ import vsc",number=100000))

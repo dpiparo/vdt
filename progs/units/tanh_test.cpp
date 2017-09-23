5f3ab9e146e76b45f55ce8fa7101d7d1cfe55e17
Paul Seyfert <pseyfert@cern.ch>
2017-09-23 19:46:47 +0200

    tweak unit test input values

--- progs/units/tanh_test.cpp
+++ progs/units/tanh_test.cpp
@@ -1,8 +1,8 @@
 /*
  * tanh_test.cpp
  *
- *  Created on: Jun 23, 2012
- *      Author: danilopiparo
+ *  Created on: Sep 23, 2017
+ *      Author: Paul Seyfert
  */
 
 #ifndef TANH_TEST_CPP_
@@ -18,12 +18,12 @@ int main(){
 	constexpr uint32_t size=10;
 	
 	// dp
-	double dpvals[size]={-1e200,-1e50,-300.,-20.,0.,13.,230.,1e20,1e303};
+	double dpvals[size]={-1e200,-1e50,-10.,-0.1,0.,0.3,2.3,1e20,1e303};
 	printFuncDiff("tanh", (dpdpfunction)tanh,(dpdpfunction)fast_tanh,dpvals,size);
 	printFuncDiff ("tanhv", (dpdpfunctionv) tanhv, (dpdpfunctionv) fast_tanhv, dpvals, size );
 
 	//sp 
-        float spvals[size]={-1e30f,-1e19f,-300.f,-20.f,0.f,13.f,230.f,1e20f,1e30f};
+        float spvals[size]={-1e30f,-1e1f,-3.f,-0.7f,0.f,.3f,2.3f,1e20f,1e30f};
         printFuncDiff("tanhf", (spspfunction)tanhf,(spspfunction)fast_tanhf,spvals,size);
         printFuncDiff ("tanhfv", (spspfunctionv) tanhfv, (spspfunctionv) fast_tanhfv, spvals, size );
 	
